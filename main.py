97d9beba593d8065dd8e4fe5ff4ab1d42861322b
alcxz <alex.zhao797@gmail.com>
2023-10-08 02:24:22 -0400

    Starting to try and work the interface

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Bro what the fuck is this thing doing am I rite")
+import streamlit as st
+import pandas as pd
+# THIS SHIT DON'T WORK
+# import os
+
+# print(pd. __version__)
+
+
+# path = os.path.dirname(__file__)
+
+# st.title("This is a title")
+
+# gamer = pd.read_excel(path + "/Ranking_Variables_-_Trade_Area_1_1.xlsx")
+# gaymer = pd.read_excel(path + "/Ranking_Variables_-_Trade_Area_2.xlsx")
+
+# gamer
+# gaymer
