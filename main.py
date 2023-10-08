d0a0f3f7271620868795d47fb118b047b67d14db
alcxz <alex.zhao797@gmail.com>
2023-10-08 12:42:27 -0400

    streamlit figured out

--- main.py
+++ main.py
@@ -1,17 +1,9 @@
 import streamlit as st
 import pandas as pd
-# THIS SHIT DON'T WORK
-# import os
-
-# print(pd. __version__)
+import os
 
 
-# path = os.path.dirname(__file__)
-
-# st.title("This is a title")
-
-# gamer = pd.read_excel(path + "/Ranking_Variables_-_Trade_Area_1_1.xlsx")
-# gaymer = pd.read_excel(path + "/Ranking_Variables_-_Trade_Area_2.xlsx")
-
-# gamer
-# gaymer
+st.write("DID I FINALLY FINISH IT????")
+dataset1 = pd.read_excel('Ranking_Variables_-_Trade_Area_1_1.xlsx')
+print(dataset1)
+st.dataframe(dataset1)
